3c5151d03752cf77f5d9a767997ba7b2bd0c8d66
Elia Nitsche <nitscheelia@gmail.com>
2026-05-18 11:53:07 +0200

    Powershell: add updateAll

--- PowerShell/alias.ps1
+++ PowerShell/alias.ps1
@@ -3,5 +3,7 @@
 
 Set-Alias lsa ls
 
+
 # using functions for aliases with arguments
 function gits { git status }
+function updateall { winget update --all --include-unknown --silent --accept-package-agreements --accept-source-agreements }
